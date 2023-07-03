a6b72f1ac0b6cd539c2cf2d954df03974738b16c
flaviano.junior <flaviano.junior@paieterno.com.br>
2023-07-03 13:23:49 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-      name: 'Flaviano Junior',
-      counter: 0
+      posts: [
+        {
+          id: 1,
+          title: 'Título 1',
+          body: 'Corpo 1'
+        },
+        {
+          id: 2,
+          title: 'Título 2',
+          body: 'Corpo 2'
+        },
+        {
+          id: 3,
+          title: 'Título 3',
+          body: 'Corpo 3'
+        },
+      ]
     };
 
   handlePClick = () => {
@@ -20,25 +35,16 @@ class App extends Component {
 
   render() {
 
-    const { name, counter } = this.state;
+    const { posts } = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={ this.handlePClick}>
-            { name } {counter}
-          </p>
-          <a
-          onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Only English
-          </a>
-        </header>
+        {posts.map(post => (
+          <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
